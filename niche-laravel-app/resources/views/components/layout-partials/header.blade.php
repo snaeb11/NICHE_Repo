3db0bb79756fc99929c176ed216b9faf95d21305
hanasplt <164708667+hanasplt@users.noreply.github.com>
2025-07-09 14:31:38 +0800

    heder n saynup

--- niche-laravel-app/resources/views/components/layout-partials/header.blade.php
+++ niche-laravel-app/resources/views/components/layout-partials/header.blade.php
@@ -1,35 +1,36 @@
-<div class="h-[10px] w-full bg-gradient-to-r from-[#D56C6C] to-[#9D3E3E]"></div>
-<nav class="flex h-[65px] w-full items-center justify-between border-b border-b-[#dddddd] bg-[#fffff0] px-6 shadow-sm">
+<div class="h-[0.5vw] w-full bg-gradient-to-r from-[#D56C6C] to-[#9D3E3E]"></div>
+<nav class="flex h-[5vw] w-full items-center justify-between border-b border-b-[#dddddd] bg-[#fffff0] px-6 shadow-sm">
     <!-- Left Side -->
-    <div class="ml-30 flex items-center space-x-3">
+    <div class="ml-[5vw] flex items-center space-x-3">
         <!-- Circle -->
         <div
-            class="flex h-[45px] w-[45px] items-center justify-center rounded-full bg-[#575757] text-sm font-semibold text-white">
+            class="flex h-[2.5vw] w-[2.5vw] items-center justify-center rounded-full bg-[#575757] text-[3vw] font-semibold text-white">
             <img src="{{ asset('assets/usep-logo.png') }}" alt="CTET Logo" class="h-full w-full object-cover" />
         </div>
 
-        <div class="h-[35px] w-[1px] bg-[#dddddd]"></div>
+        <div class="h-[3vh] w-[1px] bg-[#dddddd]"></div>
 
         <!-- Text -->
-        <span class="text-sm font-semibold text-[#575757]">University of Southeastern Philippines</span>
-
+        <span class="text-[clamp(1.3vw,1.4vw,1.3vw)] font-semibold text-[#575757]">
+            University of Southeastern Philippines
+        </span>
     </div>
 
     <!-- Right Side -->
-    <div class="mr-30 flex items-center space-x-3">
+    <div class="mr-[5vw] flex items-center space-x-3">
         <!-- Text -->
         <div class="flex flex-col items-end">
-            <span class="text-sm font-semibold leading-tight text-[#575757]">
+            <span class="text-[clamp(1.2vw,1.4vw,1.2vw)] font-semibold leading-tight text-[#575757] text-right">
                 College of Teacher Education and Technology
             </span>
-            <span class="font-regular text-xs leading-tight text-[#575757]">
+            <span class="font-regular text-[clamp(1vw,1.3vw,1vw)] leading-tight text-[#575757]">
                 Research Office
             </span>
         </div>
 
         <!-- Circle -->
         <div
-            class="flex h-[45px] w-[45px] items-center justify-center rounded-full bg-[#575757] text-sm font-semibold text-white">
+            class="flex h-[2.5vw] w-[2.5vw] items-center justify-center rounded-full bg-[#575757] text-sm font-semibold text-white">
             <img src="{{ asset('assets/ctet-logo.png') }}" alt="CTET Logo" class="h-full w-full object-cover" />
         </div>
     </div>
