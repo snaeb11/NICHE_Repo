63357811ff4f524fec231bd32a480f1332c6377e
iceuuflakes <153516200+iceuuflakes@users.noreply.github.com>
2025-07-09 09:01:24 +0800

    header adjustments

--- niche-laravel-app/resources/views/components/layout-partials/header.blade.php
+++ niche-laravel-app/resources/views/components/layout-partials/header.blade.php
@@ -18,11 +18,11 @@
     <!-- Right Side -->
     <div class="mr-30 flex items-center space-x-3">
         <!-- Text -->
-        <div class="flex flex-col text-right">
-            <span class="text-[#575757] text-sm font-semibold leading-tight">
+        <div class="flex flex-col items-end">
+            <span class="text-sm font-semibold leading-tight text-[#575757]">
                 College of Teacher Education and Technology
             </span>
-            <span class="text-[#575757] text-sm font-regular leading-tight">
+            <span class="font-regular text-xs leading-tight text-[#575757]">
                 Research Office
             </span>
         </div>
