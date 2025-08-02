8dc449d5e4fbd78fea817ce030b2ef272b176088
hanasplt <jannaesmeralda8@gmail.com>
2025-08-02 19:02:57 +0800

    removed queueing for email notif sa subs

--- niche-laravel-app/app/Notifications/SubmissionRejectedNotification.php
+++ niche-laravel-app/app/Notifications/SubmissionRejectedNotification.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class SubmissionRejectedNotification extends Notification implements ShouldQueue
+class SubmissionRejectedNotification extends Notification
 {
     use Queueable;
 
