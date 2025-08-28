d3fa06e3b1c8525639fb8f2a86c29f6bc52cfbf0
snaeb11 <suisui.vincent@gmail.com>
2025-08-28 15:58:26 +0800

    hihi

--- niche-laravel-app/resources/views/layouts/landing/index.blade.php
+++ niche-laravel-app/resources/views/layouts/landing/index.blade.php
@@ -192,14 +192,14 @@
     <script>
         document.addEventListener('contextmenu', event => event.preventDefault());
         document.addEventListener('keydown', function(e) {
-            // // Disable Ctrl+U, Ctrl+S, Ctrl+C, Ctrl+Shift+I, F12
-            // if (
-            //     (e.ctrlKey && ['u', 's', 'c'].includes(e.key.toLowerCase())) ||
-            //     (e.ctrlKey && e.shiftKey && e.key.toLowerCase() === 'i') ||
-            //     e.key === 'F12'
-            // ) {
-            //     e.preventDefault();
-            // }
+            // Disable Ctrl+U, Ctrl+S, Ctrl+C, Ctrl+Shift+I, F12
+            if (
+                (e.ctrlKey && ['u', 's', 'c'].includes(e.key.toLowerCase())) ||
+                (e.ctrlKey && e.shiftKey && e.key.toLowerCase() === 'i') ||
+                e.key === 'F12'
+            ) {
+                e.preventDefault();
+            }
         });
         document.addEventListener('keyup', function(e) {
             if (e.key === 'PrintScreen') {
