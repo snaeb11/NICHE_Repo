48253f9999ab234a1933638b19f3eda782ced8be
iceuuflakes <153516200+iceuuflakes@users.noreply.github.com>
2025-12-11 23:52:13 +0800

    moved the files from the subfolder to the root

--- niche-laravel-app/storage/framework/views/54e2d1004fa6be9af5e3de8d7736bc09.php
+++
@@ -1,5 +0,0 @@
-<div id="downloadable-files-popup-one" class="fixed inset-0 bg-black/50 items-center justify-center hidden z-50">
-    <div class="bg-white p-6 rounded-lg shadow-lg w-[90%] max-w-md space-y-4 text-center">
-        <p>penis</p>
-    </div>
-</div><?php /**PATH /home/vunc/Documents/HR_Repo/HR_Repo/niche-laravel-app/resources/views/components/popups/downloadable-files.blade.php ENDPATH**/ ?>
